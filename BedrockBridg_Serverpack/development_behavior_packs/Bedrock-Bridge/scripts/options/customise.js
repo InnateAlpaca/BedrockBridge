85cd286248076ba187680b7a31b99b86cdfb6d5a
InnateAlpaca <innatealpaca@esploratorismp.space>
2023-05-22 22:35:56 +0200

    bridge 1.2.2
    many updates and new features

--- BedrockBridg_Serverpack/development_behavior_packs/Bedrock-Bridge/scripts/options/customise.js
+++ BedrockBridg_Serverpack/development_behavior_packs/Bedrock-Bridge/scripts/options/customise.js
@@ -4,30 +4,61 @@
 // NOTE: This is the ONLY allowed modification of this addon. Any other changes to this addon's behaviour will result in a permanent ban from this service.
 // Warning: not following functions signature might break the addon. If so just get back to the original module.
 
+import { world, Player } from '@minecraft/server';
 
-import { world, Player } from '@minecraft/server'
 /**
  * @function downStreamMessage Here you can customise how messages sent from discord will appear in minecraft chat. This function will be run everytime a new message is received from your discord server.
  * @param {string} author nametag of the discord user who sent the message.
  * @param {string} message plain string containing the message sent from discord.
+ * @param {string[]} roles list of numeric identifier of discord roles for the sender.
  */
-const downStreamMessage = (author, message)=>{
+const downStreamMessage = async (author, message, roles)=>{
     world.sendMessage(`§9<${author}> ${message}§r`);
+    // world.sendMessage("", {with:"InnateALpaca102"})
 }
+
 
 /**
  * @function upStreamMessage Here you can customise how messages sent from minecraft will appear in discord chat. This function will be run everytime a player writes a message in minecraft-chat (unless it's muted or is using commands such as /w, /say...).
- * @param {Player} player player object of the sernder.
+ * @param {Player|{nick:string}} player player object of the sernder.
  * @param {string} message plain string containing the message sent by the minecraft player.
  * @returns {{author:string, text:string}}
  */
-const upStreamMessage = (player, message)=>{
+const upStreamMessage = async (player, message)=>{
     return {
-        author: player.name,
+        author: player.nick,
         text: message
-    }
+    }   
 }
+
+
+/**
+ * List of players gametag: these players' death embeds won't be shown on discord.
+ * @type {string[]}
+ */
+const ignore_death = [];
+
+/**
+ * List of players gametag: these players' log embeds (join/leave) won't be shown on discord.
+ * @type {string[]}
+ */
+const ignore_log = [];
+
+/**
+ * List of players gametag: these players' chat won't be streamed to discord.
+ * @type {string[]}
+ */
+const ignore_chat = [];
+
+// Example:
+// const ignore_death = ["TheGoodOwner123", "TheWeakDude19"]
+
+
+// const item_translate = {}
 
 export const customise = {
     downStreamMessage, upStreamMessage
+}
+export const ignores = {
+    ignore_death, ignore_log, ignore_chat
 }
