504c3130af361d00bf665d042f30cf644308665e
InnateAlpaca <innatealpaca@esploratorismp.space>
2024-03-15 21:46:35 +0100

    fixing noPing addon

--- BedrockBridg_Serverpack/development_behavior_packs/Bedrock-Bridge/scripts/bridgePlugins/basicNoPings.js
+++ BedrockBridg_Serverpack/development_behavior_packs/Bedrock-Bridge/scripts/bridgePlugins/basicNoPings.js
@@ -1,6 +1,11 @@
+/**
+ * basicNoPing v1.0.0
+ * This bridge-addon will prevent your players from sending "pinging" '@everyone' messages
+ */
+
 import { bridge } from "../addons";
 
 bridge.events.chatUpStream.subscribe(data=>{
-    data.message=data.message.replace("@everyone", "\\@everyone");
-    data.message=data.message.replace("@here", "\\@here");
+    data.message=data.message.replace("@everyone", "@*everyone*");
+    data.message=data.message.replace("@here", "@*here*");
 })
--- bridge_plugins/basicNoPings.js
+++ bridge_plugins/basicNoPings.js
@@ -6,6 +6,6 @@
 import { bridge } from "../addons";
 
 bridge.events.chatUpStream.subscribe(data=>{
-    data.message=data.message.replace("@everyone", "\\@everyone");
-    data.message=data.message.replace("@here", "\\@here");
+    data.message=data.message.replace("@everyone", "@*everyone*");
+    data.message=data.message.replace("@here", "@*here*");
 })
