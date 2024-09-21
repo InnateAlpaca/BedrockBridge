ebf5fbc93356b81c3d2a1321e178acfe1572a30b
InnateAlpaca <innatealpaca@esploratorismp.space>
2024-09-21 12:14:12 +0200

    fix TPS plugin
    Going back to the previous mechanic to improve performances

--- BedrockBridg_Serverpack/development_behavior_packs/Bedrock-Bridge/scripts/bridgePlugins/TPS.js
+++ BedrockBridg_Serverpack/development_behavior_packs/Bedrock-Bridge/scripts/bridgePlugins/TPS.js
@@ -1,56 +1,114 @@
 /**
- * TPS (Ticks Per Second) @version 1.0.2 - BedrockBridge Plugin
+ * TPS (Ticks Per Second) @version 1.0.3 - BedrockBridge Plugin
  * 
  * This bridge-addon adds a useful command to check TPS.
  * 
  * Installations: in order to insall this plugin paste this file 
  * into `Bedrock-Bridge/scripts/bridgePlugins` and add 
  * the line `import "./TPS"` to `index.js`.
+ * Admin tag is "esploratori:admin", so to be able to run commands
  * 
  * by InnateAlpaca (https://github.com/InnateAlpaca)
  * ideated by Jaso0on
  */
 
-import { system, world, DisplaySlotId } from '@minecraft/server';
+import { system, world, DisplaySlotId, MinecraftDimensionTypes, DimensionTypes } from '@minecraft/server';
 import { bridge } from '../addons';
 
-const tags = {
-    admin: "admin"
+/**Update interval (ticks) for stats shown in the scoreboard. 20 ticks is 1 second.*/
+const interval = 20;
+
+const types = {
+    items: 0,
+    players: 1,
+    mobs: 2,
+    tps: 3
 }
+
 const counters = [
-    0, 
-    0, 
+    0,
+    0,
     0,
     20
 ]
-const types = {
-    items : 0,
-    players : 1,
-    mobs : 2,
-    tps : 3
-}
-const score_names = {
-    items : "Items",
-    players : "Players",
-    mobs : "Mobs",
-    tps : "TPS"
-}
 
-const interval = 20;
+const score_names = {
+    items: "Items",
+    players: "Players",
+    mobs: "Mobs",
+    tps: "TPS"
+}
 
 const TPSscoreboard = world.scoreboard.getObjective("esploratori:serverstats")??world.scoreboard.addObjective("esploratori:serverstats", "Server Statistics");
 
-TPSscoreboard.getParticipants().forEach(p=>{
+TPSscoreboard.getParticipants().forEach(p => {
     if (!Object.values(score_names).includes(p.displayName))
         TPSscoreboard.removeParticipant(p);
 })
 
-bridge.bedrockCommands.registerCommand("tps", (user)=>{
-    if (!user.hasTag(tags.admin)){
-        user.sendMessage("§cYou are not allowed to run this command.");
-        return;
-    }
+const dimensions = {
+    [MinecraftDimensionTypes.overworld]: world.getDimension(MinecraftDimensionTypes.overworld),
+    [MinecraftDimensionTypes.nether]: world.getDimension(MinecraftDimensionTypes.nether),
+    [MinecraftDimensionTypes.theEnd]: world.getDimension(MinecraftDimensionTypes.theEnd)
+}
 
+function  getCurrentCount(filter){
+    let count = 0;
+    for (const {typeId : dim} of DimensionTypes.getAll()){
+        count+=dimensions[dim].getEntities(filter).length
+    }
+    return count;
+}
+
+/**Set the relevant counter, making sure that the actual count is syncronized with the world status*/
+function setCounters(){
+    counters[0] = getCurrentCount({ type: "minecraft:item" })
+    counters[1] = world.getAllPlayers().length
+    counters[2] = getCurrentCount({ excludeTypes: ["minecraft:item", "minecraft:player"] })
+}
+
+world.afterEvents.entitySpawn.subscribe(e=>{
+    switch (e.entity.typeId){
+        case "minecraft:item": {
+            counters[0]++
+            break;
+        }
+        case "minecraft:player": {
+            break;
+        }
+        default: {
+            counters[2]++
+        }
+    }
+})
+
+world.beforeEvents.entityRemove.subscribe(e=>{
+    switch (e.removedEntity.typeId) {
+        case "minecraft:item": {
+            counters[0]--
+            break;
+        }
+        case "minecraft:player": { 
+            break; // for some reason entity spawn doesn't work with players
+        }
+        default: {
+            counters[2]--
+        }
+    }
+})
+
+world.afterEvents.playerJoin.subscribe(() => {
+    counters[types.players]++;  
+})
+world.afterEvents.playerLeave.subscribe(() => {
+    counters[types.players]--;
+})
+
+world.afterEvents.playerSpawn.subscribe(()=>{
+    setCounters(); // player might load a new area
+})
+
+bridge.bedrockCommands.registerAdminCommand("tps", (user)=>{
     user.sendMessage("Server statistics:")
 
     user.sendMessage(`- Current TPS is: ${counters[types.tps].toFixed(2)}`)
@@ -60,23 +118,15 @@ bridge.bedrockCommands.registerCommand("tps", (user)=>{
 }, "get current TPS for the server."); //this is the description which will be visualised in !help
 
 bridge.bedrockCommands.registerAdminCommand("showServerStats", (user)=>{
-
     world.scoreboard.setObjectiveAtDisplaySlot(DisplaySlotId.Sidebar, {objective:TPSscoreboard});
-    user.sendMessage("§eServer added to sidebar.")
-    
+    user.sendMessage("§eServer added to sidebar.");    
 }, "show real-time TPS on players' screen sidebar."); //this is the description which will be visualised in !help
 
-bridge.bedrockCommands.registerCommand("hideServerStats", (user)=>{
-    if (!user.hasTag(tags.admin)){
-        user.sendMessage("§cYou are not allowed to run this command.");
-        return;
-    }
-
+bridge.bedrockCommands.registerAdminCommand("hideServerStats", (user)=>{
     world.scoreboard.clearObjectiveAtDisplaySlot(DisplaySlotId.Sidebar);
     user.sendMessage("§eServer stats removed from sidebar.")
        
 }, "hide TPS stats from players' screen."); //this is the description which will be visualised in !help
-
 
 var last_check = Date.now();
 var last_tick = system.currentTick;
@@ -88,36 +138,15 @@ system.runInterval(()=>{
 
     TPSscoreboard.setScore(score_names.tps, counters[types.tps]);
     TPSscoreboard.setScore(score_names.players, counters[types.players]);
-    TPSscoreboard.setScore(score_names.items, getEntityCount({ type: "item" }));
-    TPSscoreboard.setScore(score_names.mobs, getEntityCount({ excludeTypes: ["item", "player"] }));
+    TPSscoreboard.setScore(score_names.items, counters[types.items]);
+    TPSscoreboard.setScore(score_names.mobs, counters[types.mobs]);
 }, interval)
 
-world.afterEvents.playerJoin.subscribe(()=>{
-    counters[types.players]++;    
+
+system.run(()=>{
+    TPSscoreboard.setScore(score_names.tps, counters[types.tps]);
+    TPSscoreboard.setScore(score_names.players, counters[types.players]);
+    TPSscoreboard.setScore(score_names.items, counters[types.items]);
+    TPSscoreboard.setScore(score_names.mobs, counters[types.mobs])
 })
-world.afterEvents.playerLeave.subscribe(()=>{
-    counters[types.players]--;    
-})
-const dimensions = [
-    world.getDimension("overworld"),
-    world.getDimension("nether"),
-    world.getDimension("the_End")
-]
-async function main(){
-    TPSscoreboard.setScore(score_names.tps, 20);
-    TPSscoreboard.setScore(score_names.players, 0);
-    TPSscoreboard.setScore(score_names.mobs, 0);
-    TPSscoreboard.setScore(score_names.items, 0);
-
-    counters[types.players] = world.getAllPlayers().length
-}
-
-function getEntityCount(filter = { type: "item" }){
-    let count = 0;
-    for (const dimension of dimensions) {
-        count += dimension.getEntities(filter).length
-    }
-    return count;
-}
-
-main();
+    
--- bridge_plugins/TPS.js
+++ bridge_plugins/TPS.js
@@ -1,56 +1,114 @@
 /**
- * TPS (Ticks Per Second) v1.0.1 - BedrockBridge Plugin
+ * TPS (Ticks Per Second) @version 1.0.3 - BedrockBridge Plugin
  * 
  * This bridge-addon adds a useful command to check TPS.
  * 
  * Installations: in order to insall this plugin paste this file 
  * into `Bedrock-Bridge/scripts/bridgePlugins` and add 
  * the line `import "./TPS"` to `index.js`.
+ * Admin tag is "esploratori:admin", so to be able to run commands
  * 
  * by InnateAlpaca (https://github.com/InnateAlpaca)
  * ideated by Jaso0on
  */
 
-import { system, world, DisplaySlotId } from '@minecraft/server';
+import { system, world, DisplaySlotId, MinecraftDimensionTypes, DimensionTypes } from '@minecraft/server';
 import { bridge } from '../addons';
 
-const tags = {
-    admin: "admin"
+/**Update interval (ticks) for stats shown in the scoreboard. 20 ticks is 1 second.*/
+const interval = 20;
+
+const types = {
+    items: 0,
+    players: 1,
+    mobs: 2,
+    tps: 3
 }
+
 const counters = [
-    0, 
-    0, 
+    0,
+    0,
     0,
     20
 ]
-const types = {
-    items : 0,
-    players : 1,
-    mobs : 2,
-    tps : 3
-}
-const score_names = {
-    items : "Items",
-    players : "Players",
-    mobs : "Mobs",
-    tps : "TPS"
-}
 
-const interval = 20;
+const score_names = {
+    items: "Items",
+    players: "Players",
+    mobs: "Mobs",
+    tps: "TPS"
+}
 
 const TPSscoreboard = world.scoreboard.getObjective("esploratori:serverstats")??world.scoreboard.addObjective("esploratori:serverstats", "Server Statistics");
 
-TPSscoreboard.getParticipants().forEach(p=>{
+TPSscoreboard.getParticipants().forEach(p => {
     if (!Object.values(score_names).includes(p.displayName))
         TPSscoreboard.removeParticipant(p);
 })
 
-bridge.bedrockCommands.registerCommand("tps", (user)=>{
-    if (!user.hasTag(tags.admin)){
-        user.sendMessage("§cYou are not allowed to run this command.");
-        return;
-    }
+const dimensions = {
+    [MinecraftDimensionTypes.overworld]: world.getDimension(MinecraftDimensionTypes.overworld),
+    [MinecraftDimensionTypes.nether]: world.getDimension(MinecraftDimensionTypes.nether),
+    [MinecraftDimensionTypes.theEnd]: world.getDimension(MinecraftDimensionTypes.theEnd)
+}
 
+function  getCurrentCount(filter){
+    let count = 0;
+    for (const {typeId : dim} of DimensionTypes.getAll()){
+        count+=dimensions[dim].getEntities(filter).length
+    }
+    return count;
+}
+
+/**Set the relevant counter, making sure that the actual count is syncronized with the world status*/
+function setCounters(){
+    counters[0] = getCurrentCount({ type: "minecraft:item" })
+    counters[1] = world.getAllPlayers().length
+    counters[2] = getCurrentCount({ excludeTypes: ["minecraft:item", "minecraft:player"] })
+}
+
+world.afterEvents.entitySpawn.subscribe(e=>{
+    switch (e.entity.typeId){
+        case "minecraft:item": {
+            counters[0]++
+            break;
+        }
+        case "minecraft:player": {
+            break;
+        }
+        default: {
+            counters[2]++
+        }
+    }
+})
+
+world.beforeEvents.entityRemove.subscribe(e=>{
+    switch (e.removedEntity.typeId) {
+        case "minecraft:item": {
+            counters[0]--
+            break;
+        }
+        case "minecraft:player": { 
+            break; // for some reason entity spawn doesn't work with players
+        }
+        default: {
+            counters[2]--
+        }
+    }
+})
+
+world.afterEvents.playerJoin.subscribe(() => {
+    counters[types.players]++;  
+})
+world.afterEvents.playerLeave.subscribe(() => {
+    counters[types.players]--;
+})
+
+world.afterEvents.playerSpawn.subscribe(()=>{
+    setCounters(); // player might load a new area
+})
+
+bridge.bedrockCommands.registerAdminCommand("tps", (user)=>{
     user.sendMessage("Server statistics:")
 
     user.sendMessage(`- Current TPS is: ${counters[types.tps].toFixed(2)}`)
@@ -59,28 +117,16 @@ bridge.bedrockCommands.registerCommand("tps", (user)=>{
     user.sendMessage(`- Number of items: ${counters[types.items]}`)
 }, "get current TPS for the server."); //this is the description which will be visualised in !help
 
-bridge.bedrockCommands.registerCommand("showServerStats", (user)=>{
-    if (!user.hasTag(tags.admin)){
-        user.sendMessage("§cYou are not allowed to run this command.");
-        return;
-    }
-    system.run(()=>{
-        world.scoreboard.setObjectiveAtDisplaySlot(DisplaySlotId.Sidebar, {objective:TPSscoreboard});
-        user.sendMessage("§eServer added to sidebar.")
-    })
+bridge.bedrockCommands.registerAdminCommand("showServerStats", (user)=>{
+    world.scoreboard.setObjectiveAtDisplaySlot(DisplaySlotId.Sidebar, {objective:TPSscoreboard});
+    user.sendMessage("§eServer added to sidebar.");    
 }, "show real-time TPS on players' screen sidebar."); //this is the description which will be visualised in !help
 
-bridge.bedrockCommands.registerCommand("hideServerStats", (user)=>{
-    if (!user.hasTag(tags.admin)){
-        user.sendMessage("§cYou are not allowed to run this command.");
-        return;
-    }
-    system.run(()=>{
-        world.scoreboard.clearObjectiveAtDisplaySlot(DisplaySlotId.Sidebar);
-        user.sendMessage("§eServer stats removed from sidebar.")
-    })   
+bridge.bedrockCommands.registerAdminCommand("hideServerStats", (user)=>{
+    world.scoreboard.clearObjectiveAtDisplaySlot(DisplaySlotId.Sidebar);
+    user.sendMessage("§eServer stats removed from sidebar.")
+       
 }, "hide TPS stats from players' screen."); //this is the description which will be visualised in !help
-
 
 var last_check = Date.now();
 var last_tick = system.currentTick;
@@ -96,54 +142,11 @@ system.runInterval(()=>{
     TPSscoreboard.setScore(score_names.mobs, counters[types.mobs]);
 }, interval)
 
-const entity_types = {};
 
-world.afterEvents.entitySpawn.subscribe(({entity})=>{
-    if (entity.isValid())
-    switch(entity.typeId){
-        case "minecraft:item":{
-            entity_types[entity.id] = types.items;
-            counters[types.items]++; 
-            break;
-        }
-        default: {
-            entity_types[entity.id] = types.mobs;            
-            counters[types.mobs]++; 
-        }
-    }
+system.run(()=>{
+    TPSscoreboard.setScore(score_names.tps, counters[types.tps]);
+    TPSscoreboard.setScore(score_names.players, counters[types.players]);
+    TPSscoreboard.setScore(score_names.items, counters[types.items]);
+    TPSscoreboard.setScore(score_names.mobs, counters[types.mobs])
 })
-
-world.afterEvents.playerJoin.subscribe(()=>{
-    counters[types.players]++;    
-})
-world.afterEvents.playerLeave.subscribe(()=>{
-    counters[types.players]--;    
-})
-world.afterEvents.entityRemove.subscribe(e=>{
-    counters[entity_types[e.removedEntityId]]--;
-    delete entity_types[e.removedEntityId];    
-})
-
-async function main(){
-    TPSscoreboard.setScore(score_names.tps, 20);
-    TPSscoreboard.setScore(score_names.players, 0);
-    TPSscoreboard.setScore(score_names.mobs, 0);
-    TPSscoreboard.setScore(score_names.items, 0);
-
-    for (const dimName of ["overworld", "nether", "the_End"]){
-        const dimension = world.getDimension(dimName);
-        dimension.getEntities({type: "item"}).forEach(e=>{
-            entity_types[e.id] = types.items;
-            counters[types.items]++;
-        })
-        dimension.getEntities({excludeTypes: ["item", "player"]}).forEach(e=>{
-            entity_types[e.id] = types.items;
-            counters[types.mobs]++;
-        })
-        await null;
-    }
-
-    counters[types.players] = world.getAllPlayers().length
-}
-
-main();
+    
