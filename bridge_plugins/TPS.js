da185a47b5360eccc58a7532315957201e3e831a
InnateAlpaca <innatealpaca@esploratorismp.space>
2025-01-18 22:34:15 +0100

    tps drop alert

--- bridge_plugins/TPS.js
+++ bridge_plugins/TPS.js
@@ -1,5 +1,5 @@
 /**
- * TPS (Ticks Per Second) @version 1.0.3 - BedrockBridge Plugin
+ * TPS (Ticks Per Second) @version 1.1.0 - BedrockBridge Plugin
  * 
  * This bridge-addon adds a useful command to check TPS.
  * 
@@ -14,6 +14,14 @@
 
 import { system, world, DisplaySlotId, MinecraftDimensionTypes, DimensionTypes } from '@minecraft/server';
 import { bridge } from '../addons';
+import { bridgeDirect } from "../BridgeDirect";
+
+// report TPS lags to discord
+const discord_report_enabled = true;
+// how long before sending another report if one has just been sent (in ticks), avoid spamming
+const discord_delay = 40;
+// minimum value for TPS to trigger a report to discord
+const discord_report_min = 17;
 
 /**Update interval (ticks) for stats shown in the scoreboard. 20 ticks is 1 second.*/
 const interval = 20;
@@ -39,7 +47,7 @@ const score_names = {
     tps: "TPS"
 }
 
-const TPSscoreboard = world.scoreboard.getObjective("esploratori:serverstats")??world.scoreboard.addObjective("esploratori:serverstats", "Server Statistics");
+const TPSscoreboard = world.scoreboard.getObjective("esploratori:serverstats") ?? world.scoreboard.addObjective("esploratori:serverstats", "Server Statistics");
 
 TPSscoreboard.getParticipants().forEach(p => {
     if (!Object.values(score_names).includes(p.displayName))
@@ -52,23 +60,23 @@ const dimensions = {
     [MinecraftDimensionTypes.theEnd]: world.getDimension(MinecraftDimensionTypes.theEnd)
 }
 
-function  getCurrentCount(filter){
+function getCurrentCount(filter) {
     let count = 0;
-    for (const {typeId : dim} of DimensionTypes.getAll()){
-        count+=dimensions[dim].getEntities(filter).length
+    for (const { typeId: dim } of DimensionTypes.getAll()) {
+        count += dimensions[dim].getEntities(filter).length
     }
     return count;
 }
 
 /**Set the relevant counter, making sure that the actual count is syncronized with the world status*/
-function setCounters(){
+function setCounters() {
     counters[0] = getCurrentCount({ type: "minecraft:item" })
     counters[1] = world.getAllPlayers().length
     counters[2] = getCurrentCount({ excludeTypes: ["minecraft:item", "minecraft:player"] })
 }
 
-world.afterEvents.entitySpawn.subscribe(e=>{
-    switch (e.entity.typeId){
+world.afterEvents.entitySpawn.subscribe(e => {
+    switch (e.entity.typeId) {
         case "minecraft:item": {
             counters[0]++
             break;
@@ -82,13 +90,13 @@ world.afterEvents.entitySpawn.subscribe(e=>{
     }
 })
 
-world.beforeEvents.entityRemove.subscribe(e=>{
+world.beforeEvents.entityRemove.subscribe(e => {
     switch (e.removedEntity.typeId) {
         case "minecraft:item": {
             counters[0]--
             break;
         }
-        case "minecraft:player": { 
+        case "minecraft:player": {
             break; // for some reason entity spawn doesn't work with players
         }
         default: {
@@ -98,17 +106,17 @@ world.beforeEvents.entityRemove.subscribe(e=>{
 })
 
 world.afterEvents.playerJoin.subscribe(() => {
-    counters[types.players]++;  
+    counters[types.players]++;
 })
 world.afterEvents.playerLeave.subscribe(() => {
     counters[types.players]--;
 })
 
-world.afterEvents.playerSpawn.subscribe(()=>{
+world.afterEvents.playerSpawn.subscribe(() => {
     setCounters(); // player might load a new area
 })
 
-bridge.bedrockCommands.registerAdminCommand("tps", (user)=>{
+bridge.bedrockCommands.registerAdminCommand("tps", (user) => {
     user.sendMessage("Server statistics:")
 
     user.sendMessage(`- Current TPS is: ${counters[types.tps].toFixed(2)}`)
@@ -117,22 +125,45 @@ bridge.bedrockCommands.registerAdminCommand("tps", (user)=>{
     user.sendMessage(`- Number of items: ${counters[types.items]}`)
 }, "get current TPS for the server."); //this is the description which will be visualised in !help
 
-bridge.bedrockCommands.registerAdminCommand("showServerStats", (user)=>{
-    world.scoreboard.setObjectiveAtDisplaySlot(DisplaySlotId.Sidebar, {objective:TPSscoreboard});
-    user.sendMessage("§eServer added to sidebar.");    
+bridge.bedrockCommands.registerAdminCommand("showServerStats", (user) => {
+    world.scoreboard.setObjectiveAtDisplaySlot(DisplaySlotId.Sidebar, { objective: TPSscoreboard });
+    user.sendMessage("§eServer added to sidebar.");
 }, "show real-time TPS on players' screen sidebar."); //this is the description which will be visualised in !help
 
-bridge.bedrockCommands.registerAdminCommand("hideServerStats", (user)=>{
+bridge.bedrockCommands.registerAdminCommand("hideServerStats", (user) => {
     world.scoreboard.clearObjectiveAtDisplaySlot(DisplaySlotId.Sidebar);
     user.sendMessage("§eServer stats removed from sidebar.")
-       
+
 }, "hide TPS stats from players' screen."); //this is the description which will be visualised in !help
 
 var last_check = Date.now();
 var last_tick = system.currentTick;
 
-system.runInterval(()=>{
-    counters[types.tps] = 1000*(system.currentTick-last_tick)/(Date.now()-last_check);
+if (discord_report_enabled) {
+    bridge.events.bridgeInitialize.subscribe(e => {
+        e.registerAddition("discord_direct")
+    })
+}
+
+let last_report = system.currentTick;
+/** Report TPS drop to discord*/
+function report() {
+    const current = system.currentTick
+    if (discord_report_enabled && bridgeDirect.ready && current - last_report > discord_delay) {
+        const message = `A TPS drop has been detected on your server!\n\n- TPS: \`${Math.round(counters[types.tps])}\`\n- Entities: \`${counters[types.players] + counters[types.items] + counters[types.mobs]}\`\n- Players: ` +
+            world.getAllPlayers().map(player => `\`${player.name}\` (${player.dimension.getEntities({ maxDistance: 64, location: player.location }).length})`).join(", ")
+        bridgeDirect.sendEmbed({
+            title: "TPS Drop",
+            description: message,
+            color: 15548997
+        })
+
+        last_report = current;
+    }
+}
+
+system.runInterval(() => {
+    counters[types.tps] = 1000 * (system.currentTick - last_tick) / (Date.now() - last_check);
     last_check = Date.now();
     last_tick = system.currentTick;
 
@@ -140,13 +171,18 @@ system.runInterval(()=>{
     TPSscoreboard.setScore(score_names.players, counters[types.players]);
     TPSscoreboard.setScore(score_names.items, counters[types.items]);
     TPSscoreboard.setScore(score_names.mobs, counters[types.mobs]);
+
+    if (counters[types.tps] <= discord_report_min) {
+        report();
+    }
 }, interval)
 
 
-system.run(()=>{
+system.run(() => {
+    setCounters();
+
     TPSscoreboard.setScore(score_names.tps, counters[types.tps]);
     TPSscoreboard.setScore(score_names.players, counters[types.players]);
     TPSscoreboard.setScore(score_names.items, counters[types.items]);
     TPSscoreboard.setScore(score_names.mobs, counters[types.mobs])
 })
-    
