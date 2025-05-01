186347deea4078151f9e5c47082f82b2f913c4fe
InnateAlpaca <innatealpaca@esploratorismp.space>
2025-05-01 16:58:31 +0200

    nicer chat with name choice command

--- BedrockBridg_Serverpack/development_behavior_packs/Bedrock-Bridge/scripts/bridgePlugins/basicNicerChat.js
+++ BedrockBridg_Serverpack/development_behavior_packs/Bedrock-Bridge/scripts/bridgePlugins/basicNicerChat.js
@@ -1,12 +1,21 @@
 /**
- * Basic Nicer Chat @version 1.0.1 - BedrockBridge addon
+ * Basic Nicer Chat - BedrockBridge addon @version 1.1.0
  * 
  * This bridge-addon provides BedrockBridge with better message parsing between discord and bedrock. 
  * It will translate message formatting codes as much as possible, handle roles, channels, users, emojis...
  * 
  * by InnateAlpaca (https://github.com/InnateAlpaca)
  */
+import { world } from '@minecraft/server';
 import { bridge } from '../addons';
+
+const modes = {
+    nickname: "nickname",
+    name: "name",
+    dcName: "dcName"
+}
+
+var show_mode = world.getDynamicProperty("esploratori:nicerchat:show_mode")??modes.nickname
 
 bridge.events.chatDownStream.subscribe(e=>{
     e.message = '§r'+(e.mention?"Reply: ":"")+e.message.toBedrock(); //string.toBedrock is a custom function that handles everything is needed
@@ -17,9 +26,27 @@ bridge.events.chatUpStream.subscribe((e, sender)=>{
     e.message = e.message.toDiscord();
     e.message = e.message.replace("@everyone", "`@everyone`")
     e.message = e.message.replace("@here", "`@here`")
-    // // Sometimes you may want to visualise nametag, without all string formatting though. If so you can use the following code
-    // e.author = sender.nameTag.toDiscord(); //let's parse
-
-    // // Or yet you may want to use the discord nametag for players who have connected it. In this case you may do instead
-    // e.author = sender.dcNametag??sender.name; // not all players will have the dcNametag, we need to use name for those who don't have it.
+    
+    switch (show_mode) {
+        case modes.nickname: 
+            e.author = sender.nameTag.toDiscord(); //let's parse
+            break;
+        case modes.name:
+            e.author = sender.name.toDiscord(); //let's parse
+            break;
+        case modes.dcName:
+            e.author = (sender.dcNametag??sender.name).toDiscord(); //let's parse
+            break;
+    }
 })
+
+bridge.bedrockCommands.registerAdminCommand("changeChatNameMode", (player, mode)=>{
+    if (mode?.toString() in modes){
+        show_mode=mode.toString();
+        world.setDynamicProperty("esploratori:nicerchat:show_mode", mode.toString())
+        player.sendMessage("§eChat mode changed to "+mode)
+    }
+    else {
+        player.sendMessage(`§cMode not supported, please use one of: '${Object.values(modes).join("', '") }'. Usage: changeChatNameMode <mode>`)
+    }
+}, "Change what name is shown in discord chat: 'name', 'nickname' or 'dcName'")
